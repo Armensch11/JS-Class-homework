792b280623ff8f8ee83a8deeda73f3c425c5143c
Armensch11 <60622581+Armensch11@users.noreply.github.com>
2020-04-28 01:52:28 +0400

    Add files via upload

--- Homework 5.js
+++ Homework 5.js
@@ -16,7 +16,8 @@ function binarFind(arr, num) {
 
 // console.log(binarFind([ 1, 3, 4, 5, 9, 10, 12, 13, 15, 16, 17, 18, 19, 20, 30, 31, 32, 35, 36, 40 ], 13));
 // console.log([ 1, 3, 4, 5, 9, 10, 12, 13, 15, 16, 17, 18, 19, 20, 30, 31, 32, 35, 36, 40 ].indexOf(30));
-// let check = [ 1, 3, 4, 5, 9, 10, 12, 13, 15, 16, 17, 18, 19, 20, 30, 31, 32, 35, 36, 40 ];
+//  let check = [ 1, 3, 4, 5, 9, 10, 12, 13, 15, 16, 17, 18, 19, 20, 30, 31, 32, 35, 36, 40 ];
+// let check = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'k', 'l', 'm', 'n' ];
 function binSearchRec(arr, num, iMax = arr.length, iMin = 0, iCurrent = 0) {
 	if (arr[iCurrent] === num) {
 		return iCurrent;
@@ -31,4 +32,5 @@ function binSearchRec(arr, num, iMax = arr.length, iMin = 0, iCurrent = 0) {
 	}
 }
 
-// console.log(binSearchRec(check, 13));
+// console.log(binSearchRec(check, 'k'));
+// console.log(check.indexOf('k'));
