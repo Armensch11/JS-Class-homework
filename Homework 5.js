9556d7a393dc49a9861d53789993c1343af5ca9e
Armensch11 <60622581+Armensch11@users.noreply.github.com>
2020-04-28 01:36:52 +0400

    Add files via upload

---
+++ Homework 5.js
@@ -0,0 +1,34 @@
+function binarFind(arr, num) {
+	let iMin = 0;
+	let iMax = arr.length;
+	let iCurrent;
+
+	while (arr[iCurrent] !== num) {
+		iCurrent = Math.floor((iMax + iMin) / 2);
+		if (arr[iCurrent] >= num) {
+			iMax = iCurrent - 1;
+		} else {
+			iMin = iCurrent + 1;
+		}
+	}
+	return iCurrent;
+}
+
+// console.log(binarFind([ 1, 3, 4, 5, 9, 10, 12, 13, 15, 16, 17, 18, 19, 20, 30, 31, 32, 35, 36, 40 ], 13));
+// console.log([ 1, 3, 4, 5, 9, 10, 12, 13, 15, 16, 17, 18, 19, 20, 30, 31, 32, 35, 36, 40 ].indexOf(30));
+// let check = [ 1, 3, 4, 5, 9, 10, 12, 13, 15, 16, 17, 18, 19, 20, 30, 31, 32, 35, 36, 40 ];
+function binSearchRec(arr, num, iMax = arr.length, iMin = 0, iCurrent = 0) {
+	if (arr[iCurrent] === num) {
+		return iCurrent;
+	}
+	iCurrent = Math.floor((iMax + iMin) / 2);
+	// debugger;
+	if (arr[iCurrent] >= num) {
+		return binSearchRec(arr, num, (iMax = iCurrent - 1), iMin, iCurrent);
+	} else {
+		iMin = iCurrent + 1;
+		return binSearchRec(arr, num, iMax, (iMin = iCurrent + 1), iCurrent);
+	}
+}
+
+// console.log(binSearchRec(check, 13));
