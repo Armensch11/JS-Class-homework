cb495f1be8be47d89ceb39f92b3e8d26295cff6e
Armensch11 <60622581+Armensch11@users.noreply.github.com>
2020-05-03 10:59:30 +0400

    Add files via upload

--- Homework 5.js
+++ Homework 5.js
@@ -33,18 +33,6 @@ function binSearchRec(arr, num, iMax = arr.length, iMin = 0, iCurrent = 0) {
 
 // console.log(binSearchRec(check, 'k'));
 // console.log(check.indexOf('k'));
-function binSearchRec(arr, num, iCurrent = (iCurrent = Math.floor((arr.length - 1) / 2))) {
-	if (arr[iCurrent] === num) {
-		return iCurrent;
-	}
-
-	// debugger;
-	if (arr[iCurrent] >= num) {
-		return binSearchRec(arr, num, Math.floor((iCurrent + arr.length - 1) / 2));
-	} else {
-		return binSearchRec(arr, num, Math.floor(iCurrent / 2));
-	}
-}
 
 //mi indexov
 function binSearchRec(arr, num, iCurrent = Math.floor((arr.length - 1) / 2)) {
