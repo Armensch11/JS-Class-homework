2c2ae10ccf4c4d85f7661db411a56df5d461ffd0
Armensch11 <60622581+Armensch11@users.noreply.github.com>
2020-05-05 13:02:10 +0400

    Add files via upload

---
+++ Homework 6.js
@@ -0,0 +1,37 @@
+//Ex.1
+
+function uniqueCheck(...arr) {
+	return Array.from(new Set(arr)).length === arr.length ? true : false;
+}
+console.log(uniqueCheck('', NaN, 10, 'adfg', null, '    ', 'ef', 'Ef'));
+//=======================
+
+//Ex.2
+function evenSum(...arr) {
+	return arr.reduce((sum, item) => (!(item % 2) ? (sum += item ** 2) : (sum += 0)), 0);
+}
+// console.log(evenSum(2, 3, 4, 5, 2, 2));
+//============================
+//Ex.3
+function checkDrom(str) {
+	return [ ...str ].reverse().join('') === str ? true : false;
+}
+// console.log(checkDrom('erghhgreh'));
+
+//==========================
+//Ex.4
+
+function findMatch(word, list) {
+	let check = list.map((el) => el.split('').sort().join(''));
+	// console.log(check);
+	let checkWord = word.split('').sort().join('');
+	// console.log(checkWord);
+	// console.log(list);
+	// console.log(check);
+	let indexArr = check
+		.map((el, index) => (el === checkWord ? index : undefined))
+		.filter((index) => (index ? index : null));
+
+	return indexArr.map((el) => list[el]);
+}
+// console.log(findMatch('listen', [ 'enlists', 'itnlse', 'google', 'inlets', 'banana' ]));
